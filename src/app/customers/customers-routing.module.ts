dcc7ae3576cdd549ac0f6245160060fce708685a
Sam Zhao <zhao.shengjun@gmail.com>
2018-11-06 23:07:47 +1000

    add routing

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { CustomerListComponent } from "./customer-list/customer-list.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: CustomerListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class CustomersRoutingModule { }
+export class CustomersRoutingModule {}
--- src/app/orders/orders-routing.module.ts
+++ src/app/orders/orders-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { OrderListComponent } from "./order-list/order-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: OrderListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class OrdersRoutingModule { }
+export class OrdersRoutingModule {}
