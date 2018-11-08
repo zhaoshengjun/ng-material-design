216334036b9bf05547badd0ce4a5b209402078eb
Sam Zhao <zhao.shengjun@gmail.com>
2018-11-08 22:11:24 +1000

    tabs

--- src/app/orders/order-list/order-list.component.ts
+++ src/app/orders/order-list/order-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-order-list',
-  templateUrl: './order-list.component.html',
-  styleUrls: ['./order-list.component.css']
+  selector: "app-order-list",
+  templateUrl: "./order-list.component.html",
+  styleUrls: ["./order-list.component.css"]
 })
 export class OrderListComponent implements OnInit {
-
-  constructor() { }
+  content: string;
+  constructor() {}
 
   ngOnInit() {
+    setTimeout(() => {
+      this.content = "lazy loaded content";
+    }, 2000);
   }
-
 }
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,14 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatTabsModule } from "@angular/material/tabs";
 
-import { OrdersRoutingModule } from './orders-routing.module';
-import { OrderListComponent } from './order-list/order-list.component';
+import { OrdersRoutingModule } from "./orders-routing.module";
+import { OrderListComponent } from "./order-list/order-list.component";
 
 @NgModule({
   declarations: [OrderListComponent],
-  imports: [
-    CommonModule,
-    OrdersRoutingModule
-  ]
+  imports: [CommonModule, MatTabsModule, OrdersRoutingModule]
 })
-export class OrdersModule { }
+export class OrdersModule {}
