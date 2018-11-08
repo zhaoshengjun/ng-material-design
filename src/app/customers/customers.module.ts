d001aa8be9061fda9c765acfa582e21f4933e897
Sam Zhao <zhao.shengjun@gmail.com>
2018-11-08 22:30:17 +1000

    add card

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatCardModule } from "@angular/material/card";
 
-import { CustomersRoutingModule } from './customers-routing.module';
-import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomersRoutingModule } from "./customers-routing.module";
+import { CustomerListComponent } from "./customer-list/customer-list.component";
+import { CustomerCardComponent } from "./customer-card/customer-card.component";
 
 @NgModule({
-  declarations: [CustomerListComponent],
+  declarations: [CustomerListComponent, CustomerCardComponent],
   imports: [
     CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
     CustomersRoutingModule
   ]
 })
-export class CustomersModule { }
+export class CustomersModule {}
