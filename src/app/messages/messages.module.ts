1e405f01639d3aca18c6508fba7524b05b3b3637
Sam Zhao <zhao.shengjun@gmail.com>
2018-11-09 22:40:25 +1000

    wip

--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -1,14 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatStepperModule } from "@angular/material/stepper";
 
-import { MessagesRoutingModule } from './messages-routing.module';
-import { MessageListComponent } from './message-list/message-list.component';
+import { MessagesRoutingModule } from "./messages-routing.module";
+import { MessageListComponent } from "./message-list/message-list.component";
+import { MessageNewComponent } from "./message-new/message-new.component";
 
 @NgModule({
-  declarations: [MessageListComponent],
+  declarations: [MessageListComponent, MessageNewComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatStepperModule,
     MessagesRoutingModule
   ]
 })
-export class MessagesModule { }
+export class MessagesModule {}
