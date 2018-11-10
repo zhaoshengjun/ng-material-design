46652b47cf706c7b58b0f449e4c11316fd45e157
Sam Zhao <zhao.shengjun@gmail.com>
2018-11-10 22:45:50 +1000

    fix html issue

--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatStepperModule } from "@angular/material/stepper";
@@ -17,6 +18,7 @@ import { MessageNewComponent } from "./message-new/message-new.component";
     FormsModule,
     ReactiveFormsModule,
     MatButtonModule,
+    MatIconModule,
     MatFormFieldModule,
     MatInputModule,
     MatStepperModule,
