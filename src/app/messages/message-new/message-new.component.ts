b4458a3f1adb04393386f23d95f1342502de4e92
Sam Zhao <zhao.shengjun@gmail.com>
2018-11-10 22:51:54 +1000

    add code for stepper

--- src/app/messages/message-new/message-new.component.ts
+++ src/app/messages/message-new/message-new.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validator, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-message-new',
-  templateUrl: './message-new.component.html',
-  styleUrls: ['./message-new.component.css']
+  selector: "app-message-new",
+  templateUrl: "./message-new.component.html",
+  styleUrls: ["./message-new.component.css"]
 })
 export class MessageNewComponent implements OnInit {
-
-  constructor() { }
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  constructor(private formBuild: FormBuilder) {}
 
   ngOnInit() {
+    this.firstFormGroup = this.formBuild.group({
+      emailCtrl: ["", Validators.required]
+    });
+    this.secondFormGroup = this.formBuild.group({
+      messageCtrl: ["", Validators.required]
+    });
   }
-
 }
