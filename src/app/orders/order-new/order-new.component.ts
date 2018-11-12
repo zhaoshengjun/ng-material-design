1d442babfd3f6cf1ccf152aea9ed65fdfcafa320
Sam Zhao <zhao.shengjun@gmail.com>
2018-11-12 22:25:15 +1000

    filtered date

--- src/app/orders/order-new/order-new.component.ts
+++ src/app/orders/order-new/order-new.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-order-new',
-  templateUrl: './order-new.component.html',
-  styleUrls: ['./order-new.component.css']
+  selector: "app-order-new",
+  templateUrl: "./order-new.component.html",
+  styleUrls: ["./order-new.component.css"]
 })
 export class OrderNewComponent implements OnInit {
+  minDate = new Date();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
